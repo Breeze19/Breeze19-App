9fa51dcbad8f60a984e2992a57919216ccd43f1a
sd1998 <shashvat51@gmail.com>
2019-01-23 20:14:11 +0530

    Network checks added

--- app/src/main/java/snu/breeze/breeze19/MainActivity.java
+++ app/src/main/java/snu/breeze/breeze19/MainActivity.java
@@ -1,6 +1,8 @@
 package snu.breeze.breeze19;
 
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
 
     private FirebaseDatabase database;
+
+    private ConnectivityManager connectivityManager;
     private SharedPreferences preferences;
 
     @Override
@@ -25,31 +29,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         database = FirebaseDatabase.getInstance();
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         preferences = getSharedPreferences("breeze19",0);
         saveFCMToken();
     }
 
     private void saveFCMToken(){
-        if(preferences.getString(Constants.FCM_TOKEN_FIREBASE_KEY,"").length() == 0){
-            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
-                @Override
-                public void onSuccess(InstanceIdResult instanceIdResult) {
-                    DatabaseReference reference = database.getReference(Constants.FCM_TOKEN_FIRBASE_PATH).push();
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putString(Constants.FCM_TOKEN_KEY,instanceIdResult.getToken());
-                    editor.apply();
-                    reference.setValue(instanceIdResult.getToken()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Log.d(TAG,"FCM token saved to DB");
+        if(isConnected()) {
+            if (preferences.getString(Constants.FCM_TOKEN_FIREBASE_KEY, "").length() == 0) {
+                FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        DatabaseReference reference = database.getReference(Constants.FCM_TOKEN_FIRBASE_PATH).push();
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString(Constants.FCM_TOKEN_KEY, instanceIdResult.getToken());
+                        editor.apply();
+                        reference.setValue(instanceIdResult.getToken()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "FCM token saved to DB");
+                                }
                             }
-                        }
-                    });
-                }
-            });
-        } else{
-            Log.d(TAG,"FCM token already saved to DB");
+                        });
+                    }
+                });
+            } else {
+                Log.d(TAG, "FCM token already saved to DB");
+            }
         }
     }
+
+    private boolean isConnected(){
+        boolean connected = false;
+        if(connectivityManager != null){
+            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+            for(NetworkInfo info : networkInfo){
+                if((info.getTypeName().equalsIgnoreCase("WIFI") ||
+                        info.getTypeName().equalsIgnoreCase("MOBILE")) &&
+                        info.isConnected() && info.isAvailable()){
+                    connected = true;
+                }
+            }
+        }
+        return connected;
+    }
+
 }
