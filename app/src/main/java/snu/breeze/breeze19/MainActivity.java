a8b9759fddd9896efed81ca1af51389e029c8c33
mudit9 <msgagent1@gmail.com>
2019-01-30 19:25:02 +0530

    bottommenu

--- app/src/main/java/snu/breeze/breeze19/MainActivity.java
+++ app/src/main/java/snu/breeze/breeze19/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter pagerAdapter;
+    MenuItem prevMenuItem;
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,37 +42,91 @@ public class MainActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         preferences = getSharedPreferences("breeze19",0);
-        saveFCMToken();
+        viewPager =  findViewById(R.id.view_pager);
 
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        tabLayout = (TabLayout) findViewById(R.id.main_tab_layout);
+        saveFCMToken();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.events:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.maps:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.contact:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),true);
         viewPager.setAdapter(pagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void saveFCMToken(){
-        if(isConnected()) {
+        Log.e(TAG,"Saving FCM");
+
+        if(true) {
             if (preferences.getString(Constants.FCM_TOKEN_FIREBASE_KEY, "").length() == 0) {
+                Log.e(TAG, "FCM token saving to DB");
                 FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
                     @Override
                     public void onSuccess(InstanceIdResult instanceIdResult) {
+                        Log.e(TAG, "FCM token saving here to DB");
                         DatabaseReference reference = database.getReference(Constants.FCM_TOKEN_FIREBASE_PATH).push();
                         SharedPreferences.Editor editor = preferences.edit();
                         editor.putString(Constants.FCM_TOKEN_KEY, instanceIdResult.getToken());
                         editor.apply();
+                        Log.e(TAG, Constants.FCM_TOKEN_KEY);
                         reference.setValue(instanceIdResult.getToken()).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
+                                Log.e(TAG, "FCM token saving1 to DB");
                                 if (task.isSuccessful()) {
-                                    Log.d(TAG, "FCM token saved to DB");
+                                    Log.e(TAG, "FCM token saved to DB");
+                                }
+                                else{
+                                    Log.e(TAG,"Task unsuccessful");
                                 }
                             }
                         });
                     }
                 });
             } else {
-                Log.d(TAG, "FCM token already saved to DB");
+                Log.e(TAG, "FCM token already saved to DB");
             }
         }
     }
